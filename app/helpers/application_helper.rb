e581c6914f6a2907212645bf1920bec05a065636
Lukas Elmer <lukas.elmer@gmail.com>
2014-05-15 17:38:24 +0200

    bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def page_title
+    'Hello!'
+  end
+
+  def navi_link(name, path)
+    content_tag('li', link_to(name, path), class: (current_page?(path) ? 'active' : nil))
+  end
 end
