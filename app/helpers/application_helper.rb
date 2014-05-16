c985318e01b6a8a04bc3f2e1cad320b8d4c3d051
Lukas Elmer <lukas.elmer@gmail.com>
2014-05-16 22:25:01 +0200

    gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,9 @@ module ApplicationHelper
   def navi_link(name, path)
     content_tag('li', link_to(name, path), class: (current_page?(path) ? 'active' : nil))
   end
+
+  def gravatar_url(user, size = 50)
+    gravatar_hash = Digest::MD5.hexdigest(user.email.downcase)
+    "https://gravatar.com/avatar/#{gravatar_hash}?s=#{size}"
+  end
 end
